bc437817dabe74f6ec6de50cdc442d0111de6028
YaroslavOvdii <yaroslav.o@upplabs.com>
2020-01-27 16:44:54 +0200

    Changed to instance

--- js/build.js
+++ js/build.js
@@ -1,19 +1,17 @@
 Fliplet.Navigator.onReady().then(function() {
-  $('[data-rss-id]').each(function(){
-    var container = this;
-    var id = $(this).data('rss-id');
-    var uuid = $(this).data('rss-uuid');
-    var config = Fliplet.Widget.getData(id);
+  Fliplet.Widget.instance('rss', function(config) {
 
     if (!window.rssConf) {
         window.rssConf = [];
     }
+
     window.rssConf.push(config);
 
     window.reloadTime = null;
 
     // multiple event prevention
     var currentTime = new Date();
+
     if (currentTime - window.reloadTime < 1000) {
         return;
     }
@@ -25,5 +23,5 @@ Fliplet.Navigator.onReady().then(function() {
     }
 
     window.reloadTime = currentTime;
-    });
+  });
 });
